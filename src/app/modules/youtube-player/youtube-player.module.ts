eb310811612f79dc65bd33fbb3b59ae3d1b84058
Marco Bianco <yale01@gmail.com>
2020-03-14 12:01:57 +0100

    reorganize folders

--- src/app/modules/youtube-player/youtube-player.module.ts
+++ src/app/modules/youtube-player/youtube-player.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YouTubePlayerModule } from '@angular/youtube-player';
-import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { YoutubePlayerComponent } from './components/youtube-player/youtube-player.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
